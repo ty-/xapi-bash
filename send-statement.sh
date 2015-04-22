a90e2994cd07fe30a83e10755fc575d8737e6066
Tyler Mulligan <tyler.mulligan.ctr@adlnet.gov>
2015-04-22 09:49:38 -0400

    basic example of sending a statement with curl

---
+++ send-statement.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ENDPOINT="https://lrs.adlnet.gov/xapi/statements"
+
+# Have bash encode the credentials to base64
+USER="xapi-tools"
+PASS="xapi-tools"
+AUTH=$(echo -n "$USER:$PASS" | base64)
+
+# Alrady encoded AUTH 
+#AUTH="eGFwaS10b29sczp4YXBpLXRvb2xz"
+
+
+STMT='{
+    "actor": {
+        "mbox": "mailto:tyler.mulligan.ctr@adlnet.gov",
+        "name": "Tyler",
+        "objectType": "Agent"
+    },
+    "verb": {
+        "id": "http://adlnet.gov/expapi/verbs/passed",
+        "display": {
+            "en-US": "passed"
+        }
+    },
+    "object": {
+        "id": "http://adlnet.gov/expapi/activities/example",
+        "definition": {
+            "name": {
+                "en-US": "Example Activity"
+            },
+            "description": {
+                "en-US": "Example activity description"
+            }
+        },
+        "objectType": "Activity"
+    }
+}'
+
+curl -i -X POST -H "X-Experience-API-Version: 1.0.1" -H "Authorization: Basic $AUTH" --data-binary "$STMT" $ENDPOINT
